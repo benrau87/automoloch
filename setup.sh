cdd0e0d6c4abd8af5a8b3d66efaf727279211adc
benrau87 <benrau87@users.noreply.github.com>
2017-05-08 09:30:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,16 +72,116 @@ fi
 ########################################
 ##BEGIN MAIN SCRIPT##
 #Pre checks: These are a couple of basic sanity checks the script does before proceeding.
+apt-get -y install apt-transport-https
+
+##Java
+add-apt-repository ppa:webupd8team/java -y 
+
+#Nodejs
+#curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+
+##Elasticsearch
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - 
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+apt-get update
+
 cd ~
 
-git clone https://github.com/aol/moloch.git
+ES=5.4
+NODEJS=6.10.3
+INSTALL_DIR=$PWD
 
-git clone https://github.com/benrau87/MolochSetup.git
+echo -n "Looking for java "
+which java
+JAVA_VAL=$?
 
-cp MolochSetup/Moloch\ Script.sh ~/moloch/
+if [ $JAVA_VAL -ne 0 ]; then
+echo -n "java command not found, real Java 8 is recommended for this install, would you like to install it now? [yes] "
+read INSTALLJAVA
+if [ -n "$INSTALLJAVA" -a "x$INSTALLJAVA" != "xyes" ]; then
+echo "Install java and try again"
+exit
+fi
 
-cd moloch
+if [ -f "/etc/debian_version" ]; then
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+  sudo debconf-set-selections
+apt-get install oracle-java7-installer -y
+if [ $? -ne 0 ]; then
+echo "ERROR - 'apt-get install java7' failed"
+exit
+fi
 
-bash Moloch\ Script.sh
+if [ "x$http_proxy" != "x" ]; then
+JAVA_OPTS="$JAVA_OPTS `echo $http_proxy | sed 's/https*:..\(.*\):\(.*\)/-Dhttp.proxyHost=\1 -Dhttp.proxyPort=\2/'`"
+export JAVA_OPTS
+echo "Because http_proxy is set ($http_proxy) setting JAVA_OPTS to ($JAVA_OPTS)"
+sleep 1
+fi
+
+if [ "x$https_proxy" != "x" ]; then
+JAVA_OPTS="$JAVA_OPTS `echo $https_proxy | sed 's/https*:..\(.*\):\(.*\)/-Dhttps.proxyHost=\1 -Dhttps.proxyPort=\2/'`"
+export JAVA_OPTS
+echo "Because https_proxy is set ($https_proxy) setting JAVA_OPTS to ($JAVA_OPTS)"
+sleep 1
+fi
+
+##Pfring
+echo -n "Use pfring? ('yes' enables) [no] "
+read USEPFRING
+PFRING=""
+if [ -n "$USEPFRING" -a "x$USEPFRING" = "xyes" ]; then
+echo "MOLOCH - Using pfring - Make sure to install the kernel modules"
+sleep 1
+PFRING="--pfring"
+fi
+
+## ElasticSearch
+echo "MOLOCH: Downloading and installing elastic search"
+if [ ! -f "elasticsearch-${ES}.tar.gz" ]; then
+apt-get install -y elasticsearch
+systemctl daemon-reload 
+systemctl enable elasticsearch.service
+systemctl start elasticsearch.service 
+fi
+
+# NodeJS
+echo "MOLOCH: Downloading and installing node"
+cd ${INSTALL_DIR}/thirdparty
+if [ ! -f "node-v${NODEJS}.tar.gz" ]; then
+wget http://nodejs.org/dist/v${NODEJS}/node-v${NODEJS}.tar.gz
+fi
+
+tar xfz node-v${NODEJS}.tar.gz
+cd node-v${NODEJS}
+./configure
+make
+make install
+./configure --prefix=${TDIR}
+make install
+
+if [ "x$http_proxy" != "x" ]; then
+${TDIR}/bin/npm config set proxy $http_proxy
+echo "Because http_proxy is set ($http_proxy) setting npm proxy"
+sleep 1
+fi
+
+if [ "x$https_proxy" != "x" ]; then
+${TDIR}/bin/npm config set https-proxy $https_proxy
+echo "Because https_proxy is set ($https_proxy) setting npm https-proxy"
+sleep 1
+fi
+
+
+#git clone https://github.com/aol/moloch.git
+
+#git clone https://github.com/benrau87/MolochSetup.git
+
+#cp MolochSetup/Moloch\ Script.sh ~/moloch/
+
+#cd moloch
+
+#bash Moloch\ Script.sh
 
 
